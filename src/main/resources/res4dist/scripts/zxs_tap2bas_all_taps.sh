4f4af645936b6ba0c662d2f614f177ace3bc6871
Martin Pokorný <MartinPokorny.czech@gmail.com>
2017-10-30 15:51:53 +0100

    skript pro hromadny prevod

--- src/main/resources/res4dist/scripts/zxs_tap2bas_all_taps.sh
+++ src/main/resources/res4dist/scripts/zxs_tap2bas_all_taps.sh
@@ -18,7 +18,7 @@ cd - > /dev/null
 for i in *.tap; do
     out="$(cutExt $i).bas"
     echo " * $i -> $out"
-    java -Djava.util.logging.config.file="$script_dir/logging.properties" -jar "$script_dir/zxs_tap2bas.jar" -i $i --onlyBasic -o $out
-    #java -Djava.util.logging.config.file="$script_dir/logging.properties" -jar "$script_dir/zxs_tap2bas.jar" -i $i -o $out
+    #java -Djava.util.logging.config.file="$script_dir/logging.properties" -jar "$script_dir/zxs_tap2bas.jar" -i $i --onlyBasic -o $out
+    java -Djava.util.logging.config.file="$script_dir/logging.properties" -jar "$script_dir/zxs_tap2bas.jar" -i $i -o $out
 done
 
